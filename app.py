74df910d02d24d380a7d97850434eb112aed257a
shifthing <41026420+shifthing@users.noreply.github.com>
2025-09-21 15:58:49 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+        return '¡Hola, mundo! Esta es mi primera app en la nube. Versión 1.0.'
+
+if __name__ == '__main__':
+    # La aplicación debe escuchar en el puerto 8080 para Heroku
+    app.run(host='0.0.0.0', port=8080)
